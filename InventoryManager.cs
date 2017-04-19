e3987fedc22bc86cceb751d3363b532c598d502b
Not-Steve1 <fernandeznico26@yahoo.com>
2017-04-19 11:15:55 -0400

    Add files via upload

---
+++ InventoryManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    private GameObject[,] passObjectArr;
+    public GameObject highlightedSlot;
+
+    private int checkInt;
+    public IntVector2 otherItemPos = IntVector2.zero;
+
+    private void Start()
+    {
+        passObjectArr = PanelScript.slotGrid;
+        
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            checkInt = SlotScript.checkInt;
+            if (highlightedSlot != null && ItemScript.selectedItem != null) //check if drop/swap
+            {
+                switch (checkInt)
+                {
+                    case 0: //store on empty slots
+                        StoreItem(ItemScript.selectedItem);
+                        ItemScript.isDragging = false;
+                        ItemScript.selectedItem = null;
+                        SlotScript.itemSize = IntVector2.zero;
+                        break;
+                    case 1: //swap items
+                        ItemScript.selectedItem = SwapItem(ItemScript.selectedItem);
+                        ItemScript.selectedItem.GetComponent<CanvasGroup>().alpha = 0.5f;
+                        ItemScript.isDragging = true;
+                        break;
+                }
+            }
+            else if (highlightedSlot != null && ItemScript.selectedItem == null && highlightedSlot.GetComponent<SlotScript>().storedItem != null) //check if retrieving item
+            {
+                ItemScript.selectedItem = GetItem(highlightedSlot);
+                ItemScript.selectedItem.GetComponent<CanvasGroup>().alpha = 0.5f;
+                ItemScript.isDragging = true;
+            }
+        }
+    }
+
+    private void StoreItem(GameObject item)
+    {
+        SlotScript instanceScript;
+        IntVector2 itemSize = ItemScript.selectedItem.GetComponent<ItemScript>().itemSize;
+        IntVector2 gridPos = highlightedSlot.GetComponent<SlotScript>().gridPos;
+
+        for (int y = 0; y < itemSize.y; y++)
+        {
+            for (int x = 0; x < itemSize.x; x++)
+            {
+                //set each slot parameters
+                instanceScript = passObjectArr[x + gridPos.x, y + gridPos.y].GetComponent<SlotScript>();
+                instanceScript.storedItem = item;
+                instanceScript.storedItemSize = itemSize;
+                instanceScript.itemPos = gridPos;
+                instanceScript.isOccupied = true;
+                passObjectArr[x + gridPos.x, y + gridPos.y].GetComponent<Image>().color = Color.white;
+
+                ItemScript.selectedItem.transform.SetParent(GameObject.Find("ItemAnchor").transform);
+                ItemScript.selectedItem.transform.position = highlightedSlot.transform.position;
+                ItemScript.selectedItem.GetComponent<CanvasGroup>().alpha = 0.75f;
+            }
+        }
+
+    }
+
+    private GameObject GetItem(GameObject slotObject)
+    {
+        SlotScript highlightedItemScript = slotObject.GetComponent<SlotScript>();
+        GameObject retItem = highlightedItemScript.storedItem;
+
+        IntVector2 itemSize = highlightedItemScript.storedItemSize;
+        IntVector2 itemPos = highlightedItemScript.itemPos;
+        SlotScript.itemSize = itemSize;
+
+        SlotScript instanceScript;
+
+        for (int y = 0; y < itemSize.y; y++)
+        {
+            for (int x = 0; x < itemSize.x; x++)
+            {
+                //reset each slot parameters
+                instanceScript = passObjectArr[x + itemPos.x, y + itemPos.y].GetComponent<SlotScript>();
+                instanceScript.storedItem = null;
+                instanceScript.storedItemSize = IntVector2.zero;
+                instanceScript.itemPos = IntVector2.zero;
+                instanceScript.isOccupied = false;
+            }
+        }
+        if (checkInt == 0)
+        {
+            slotObject.GetComponent<SlotScript>().GridColorChange(new Color32(180, 255, 180, 255), true);
+        }
+        
+        return retItem;
+    }
+
+    private GameObject SwapItem(GameObject item)
+    {
+        GameObject tempItem;
+        tempItem = GetItem(passObjectArr[otherItemPos.x ,otherItemPos.y]); //retrieve and store item on temp
+        StoreItem(item); //store selectedItem
+        return tempItem;
+    }
+
+
+}
---
+++ PanelScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PanelScript : MonoBehaviour
+{
+    /*to do list
+     * add panels (done)
+     * dynamic inventory functions (done)
+     * make test items (done)
+     * move items   (done)  
+     * drop mechanics   (done)
+     * improve move items mechanics (done)
+     * make scroll list UI for items
+     * drop items back list
+     * create item generator
+     * add  delete item panel
+     * add item stat
+     * add item stat overlay
+     * make random item generator *later*
+     *
+     * have item stat affect player stats *later*
+     * create random item stat *maybe* *hard*
+     *
+     * 
+     */
+
+    /*optionals
+     * add graphics
+     * item rotate
+     * save/load function *hard*
+     * improve IntVector2 methods and parameters
+     */
+
+
+
+    //public static List<GameObject> //may of use later
+    public static GameObject[,] slotGrid;
+    public GameObject slotPrefab;
+    public GameObject anchorPrefab;
+    
+    public int row, column;
+    public float slotSize;
+    public float edgePadding;
+
+
+    //test vars
+    
+
+    public void Awake()
+    {
+        slotGrid = new GameObject[column, row];
+        ResizePanel(this.gameObject);
+        CreateSlots();
+        ResizePanel(anchorPrefab);
+    }
+
+   
+
+
+
+    private void CreateSlots()
+    {
+        GetComponent<GridLayoutGroup>().cellSize = Vector2.one * slotSize;
+        for (int y = 0; y < row; y++)
+        {
+            for (int x = 0; x < column; x++)
+            {
+
+                slotGrid[x, y] = (GameObject)Instantiate(slotPrefab);          
+                slotGrid[x, y].transform.name = "slot[" + x + "," + y + "]";
+                slotGrid[x, y].transform.SetParent(this.transform);
+                slotGrid[x, y].GetComponent<RectTransform>().localScale = Vector3.one;
+                slotGrid[x, y].GetComponent<SlotScript>().gridPos = new IntVector2(x, y);
+            }
+        }
+        
+    }
+
+    private void ResizePanel(GameObject obj)
+    {
+        float width, height;
+        width = (column * slotSize) + (obj == this.gameObject ? (edgePadding * 2):0);
+        height = (row * slotSize) + (obj == this.gameObject ? (edgePadding * 2) : 0);
+        
+        RectTransform rect = obj.GetComponent<RectTransform>();
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+        rect.localScale = Vector3.one;
+    }
+}
