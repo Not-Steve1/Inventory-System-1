b5ea5eb6cc4a347518f857e9caa6da69e92ffeab
Not-Steve1 <fernandeznico26@yahoo.com>
2017-04-19 11:19:11 -0400

    Add files via upload

---
+++ Scripts/IntVector2.cs
@@ -0,0 +1,69 @@
+﻿public struct IntVector2
+{
+    public int x, y;
+
+    public IntVector2(int num1, int num2)
+    {
+        x = num1;
+        y = num2;
+    }
+
+    public static IntVector2 one
+    {
+        get { return new IntVector2(1, 1); }
+        
+    }
+
+    public static IntVector2 oneNeg
+    {
+        get { return new IntVector2(-1, -1); }
+
+    }
+
+    public static IntVector2 zero
+    {
+        get{ return new IntVector2(0, 0); }
+    }
+
+    public static IntVector2 operator +(IntVector2 a, IntVector2 b)
+    {
+        IntVector2 ret;
+
+        ret.x = a.x + b.x;
+        ret.y = a.y + b.y;
+        return ret;
+    }
+
+    public static bool operator ==(IntVector2 a, IntVector2 b)
+    {
+        if ((a.x == b.x) && (a.x == b.x))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool operator !=(IntVector2 a, IntVector2 b)
+    {
+        if ((a.x != b.x) && (a.x != b.x))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public override bool Equals(object o)
+    {
+       return true;
+    }
+    public override int GetHashCode()
+    {
+        return 0;
+    }
+
+}
---
+++ Scripts/ItemScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ItemScript : MonoBehaviour ,IPointerClickHandler, IPointerExitHandler,IPointerEnterHandler{
+
+    public int itemL, itemW;   
+    public IntVector2 itemSize;
+
+    
+    private GameObject highlightedItem = null;
+    public static GameObject selectedItem = null;
+    private CanvasGroup itemCanvas;
+    public static bool isDragging = false;
+    private float slotSize;
+    private Vector3 dragOffset;
+
+    private void Start()
+    {
+        itemSize = new IntVector2(itemL, itemW);
+        itemCanvas = GetComponent<CanvasGroup>();
+        slotSize = GameObject.Find("InvenPanel").GetComponent<PanelScript>().slotSize;
+        dragOffset = new Vector3(-slotSize / 2, slotSize / 2, 0);
+    }
+
+    //drag selected item
+    private void Update()
+    {
+        if (isDragging)
+        {
+            selectedItem.transform.position = Input.mousePosition + dragOffset;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        highlightedItem = this.gameObject;      
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        highlightedItem = null;
+    }
+
+    //mouse pick up item
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if ((highlightedItem != null) &&(selectedItem == null))
+        {
+            selectedItem = highlightedItem;
+            isDragging = true;
+            SlotScript.itemSize = itemSize;
+            itemCanvas.alpha = 0.5f;
+            itemCanvas.blocksRaycasts = false;
+            
+        }
+        
+    }
+
+    
+}
